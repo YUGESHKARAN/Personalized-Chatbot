5765f8b5ca40aaaf6f3f483c4c2cc02314ee47f7
YUGESHKARAN <yugeshkaran01@gmail.com>
2024-04-23 14:13:58 +0530

    groq api added

--- chat.py
+++ chat.py
@@ -2,11 +2,13 @@ import streamlit as st
 import openai
 import os
 #from dotenv import load_dotenv
+#from langchain_openai import ChatOpenAI
 #load_dotenv()
 
 
 #LANGCHAIN_API_KEY=ls__d543df545ab54ddf943afd9a4e62aeaf
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+#openai.api_key = st.secrets["OPENAI_API_KEY"]
+#os.environ['OPENAI_API_KEY'] = "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"
 st.markdown("""<h1 style="text-align:center;color:#55E6C1;">welcome to <span style='color:red;'> Chatrobot </span> </h1>""",unsafe_allow_html=True)
 
 st.markdown("""
